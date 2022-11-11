6fb88bcaea05f01c655a0c24807f8f1a4f4ace53
k-raky <kaneraky12@gmail.com>
2022-11-11 22:37:53 +0000

    tp added

---
+++ Banque/CompteBancaire.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Xml;
+
+namespace CompteBanqueNS;
+
+public class CompteBancaire
+{
+    private string m_nomClient;
+    private double m_solde;
+    private bool m_bloque = false;
+
+    private CompteBancaire() { }
+
+    public CompteBancaire(string nomclient, double solde)
+    {
+        m_nomClient = nomclient;
+        m_solde = solde;
+    }
+
+    public string nomClient
+    {
+        get { return m_nomClient; }
+    }
+
+    public double Balance
+    {
+        get { return m_solde; }
+    }
+
+    public void Débiter(double montant)
+    {
+
+        if (m_bloque)
+        {
+            throw new Exception("Compte bloqué");
+        }
+
+        if (montant > m_solde)
+        {
+            throw new ArgumentOutOfRangeException("Montant débité doit être supérieur ou egal au solde disponible");
+        }
+
+        if (montant < 0)
+        {
+            throw new ArgumentOutOfRangeException("Montant doit être positif");
+        }
+
+        m_solde -= montant; // code intentionellement faux
+    }
+
+    public void Crediter(double montant)
+    {
+
+        if (m_bloque)
+        {
+            throw new Exception("Compte bloqué");
+        }
+
+        if (montant < 0)
+        {
+            throw new ArgumentOutOfRangeException("Montant débité doit être supérieur a 0");
+        }
+
+        m_solde += montant;
+    }
+
+    private void BloquerCompte()
+    {
+        m_bloque = true;
+    }
+
+    private void DébloquerCompte()
+    {
+        m_bloque = false;
+    }
+
+    public static void Main()
+    {
+        CompteBancaire cb = new CompteBancaire("Pr. Abdoulaye Diankha", 500000);
+
+        cb.Crediter(500000);
+        cb.Débiter(400000);
+        Console.WriteLine("Solde disponible égal à F{0}", cb.Balance);
+    }
+
+}
+
+
+
---
+++ BanqueTests/CompteBancaireTests.cs
@@ -0,0 +1,55 @@
+﻿using CompteBanqueNS;
+namespace BanqueTests;
+
+[TestClass]
+public class CompteBancaireTests
+{
+    [TestMethod]
+    public void VérifierDébitCompteCorrect()
+    {
+        // Ouvrir un compte
+        double soldeInitial = 500000;
+        double montantDébit = 400000;
+        double soldeAttendu = 100000;
+        var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+        // Débiter
+        compte.Débiter(montantDébit);
+        // Tester
+        double solde0btenu = compte.Balance;
+        Assert.AreEqual(soldeAttendu, solde0btenu, 0.001, "Compte débité incorrectement");
+    }
+
+    [TestMethod]
+    [ExpectedExceptionAttribute(typeof(ArgumentOutOfRangeException))]
+    public void DébiterMontantNégatifLèveArgumentOutOfRange()
+    {
+        // Ouvrir un compte
+        double soldeInitial = 500000;
+        double montantDébit = -400000;
+        double soldeAttendu = 100000;
+        var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+        // Débiter
+        compte.Débiter(montantDébit);
+        // Tester
+        double solde0btenu = compte.Balance;
+        Assert.AreEqual(soldeAttendu, solde0btenu, 0.001, "Compte débité incorrectement");
+    }
+
+    [TestMethod]
+    [ExpectedExceptionAttribute(typeof(ArgumentOutOfRangeException))]
+    public void DébiterMontantSupérieurSoldeLèveArgumentOutOfRange()
+    {
+        // Ouvrir un compte
+        double soldeInitial = 500000;
+        double montantDébit = 700000;
+        double soldeAttendu = 100000;
+        var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+        // Débiter
+        compte.Débiter(montantDébit);
+        // Tester
+        double solde0btenu = compte.Balance;
+        Assert.AreEqual(soldeAttendu, solde0btenu, 0.001, "Compte débité incorrectement");
+    }
+
+
+}
